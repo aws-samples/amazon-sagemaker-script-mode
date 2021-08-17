095af07488889bb2655b741749d8740d3e11a49e
kthadaka <kthadaka!>
2021-08-17 15:01:12 +0000

    Updated code with async inference

--- hugging-face-lambda-step/lambda_deployer.py
+++ hugging-face-lambda-step/lambda_deployer.py
@@ -1,4 +1,3 @@
-
 """
 This Lambda function creates an Endpoint Configuration and deploys a model to an Endpoint. 
 The name of the model to deploy is provided via the `event` argument
@@ -18,33 +17,37 @@ def lambda_handler(event, context):
     endpoint_config_name = event["endpoint_config_name"]
     endpoint_name = event["endpoint_name"]
     role = event["role"]
-    
-    container = {
-        'ModelPackageName': model_package_arn
-    }
-    
+    bucket = event["bucket"]
+    s3_prefix = event["s3_prefix"]
+
+    container = {"ModelPackageName": model_package_arn}
+
     create_model_respose = sm_client.create_model(
-        ModelName = model_name,
-        ExecutionRoleArn = role,
-        Containers = [container]
+        ModelName=model_name, ExecutionRoleArn=role, Containers=[container]
     )
-    
+
     create_endpoint_config_response = sm_client.create_endpoint_config(
         EndpointConfigName=endpoint_config_name,
         ProductionVariants=[
             {
-                "InstanceType": "ml.m4.xlarge",
-                "InitialVariantWeight": 1,
-                "InitialInstanceCount": 1,
+                "VariantName": "variant1",
                 "ModelName": model_name,
-                "VariantName": "AllTraffic",
+                "InstanceType": "ml.m5.xlarge",
+                "InitialInstanceCount": 1
             }
         ],
+        AsyncInferenceConfig={
+            "OutputConfig": {
+                "S3OutputPath": f"s3://{s3_bucket}/{s3_prefix}/output",
+            },
+            "ClientConfig": {
+                "MaxConcurrentInvocationsPerInstance": 4
+            }
+        }
     )
 
     create_endpoint_response = sm_client.create_endpoint(
-        EndpointName=endpoint_name, 
-        EndpointConfigName=endpoint_config_name
+        EndpointName=endpoint_name, EndpointConfigName=endpoint_config_name
     )
 
     return {
